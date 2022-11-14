0351aaf5c3c9e96b7c1ffc605a32de76b2da2aa3
Emmanuel ZIMMERT <ez@ladigitale.dev>
2022-11-14 18:34:04 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3509,100 +3509,98 @@ function exporterPadsJson () {
 				const id = pad.substring(5)
 				const chemin = path.join(__dirname, '..', '/static/pads')
 				db.hgetall('pads:' + id, function (err, donnees) {
-					fs.exists(path.normalize(chemin + '/' + id + '.json'), function (existe) {
-						if (existe === false && ((donnees.hasOwnProperty('modifie') && moment(donnees.modifie).isBefore(moment().subtract(10, 'days'))) || (donnees.hasOwnProperty('date') && moment(donnees.date).isBefore(moment().subtract(10, 'days'))))) {
-							const donneesPad = new Promise(function (resolveMain) {
-								db.hgetall('pads:' + id, function (err, resultats) {
-									if (err) { resolveMain({}) }
-									resolveMain(resultats)
-								})
+					if ((donnees.hasOwnProperty('modifie') && moment(donnees.modifie).isBefore(moment().subtract(10, 'days'))) || (donnees.hasOwnProperty('date') && moment(donnees.date).isBefore(moment().subtract(10, 'days')))) {
+						const donneesPad = new Promise(function (resolveMain) {
+							db.hgetall('pads:' + id, function (err, resultats) {
+								if (err) { resolveMain({}) }
+								resolveMain(resultats)
 							})
-							const blocsPad = new Promise(function (resolveMain) {
-								const donneesBlocs = []
-								db.zrange('blocs:' + id, 0, -1, function (err, blocs) {
-									if (err) { resolveMain(donneesBlocs) }
-									for (const bloc of blocs) {
-										const donneesBloc = new Promise(function (resolve) {
-											db.hgetall('pad-' + id + ':' + bloc, function (err, donnees) {
-												if (err) { resolve({}) }
-												if (donnees && Object.keys(donnees).length > 0) {
-													const donneesCommentaires = []
-													db.zrange('commentaires:' + bloc, 0, -1, function (err, commentaires) {
+						})
+						const blocsPad = new Promise(function (resolveMain) {
+							const donneesBlocs = []
+							db.zrange('blocs:' + id, 0, -1, function (err, blocs) {
+								if (err) { resolveMain(donneesBlocs) }
+								for (const bloc of blocs) {
+									const donneesBloc = new Promise(function (resolve) {
+										db.hgetall('pad-' + id + ':' + bloc, function (err, donnees) {
+											if (err) { resolve({}) }
+											if (donnees && Object.keys(donnees).length > 0) {
+												const donneesCommentaires = []
+												db.zrange('commentaires:' + bloc, 0, -1, function (err, commentaires) {
+													if (err) { resolve(donnees) }
+													for (let commentaire of commentaires) {
+														donneesCommentaires.push(JSON.parse(commentaire))
+													}
+													donnees.commentaires = donneesCommentaires.length
+													donnees.listeCommentaires = donneesCommentaires
+													db.zrange('evaluations:' + bloc, 0, -1, function (err, evaluations) {
 														if (err) { resolve(donnees) }
-														for (let commentaire of commentaires) {
-															donneesCommentaires.push(JSON.parse(commentaire))
-														}
-														donnees.commentaires = donneesCommentaires.length
-														donnees.listeCommentaires = donneesCommentaires
-														db.zrange('evaluations:' + bloc, 0, -1, function (err, evaluations) {
-															if (err) { resolve(donnees) }
-															const donneesEvaluations = []
-															evaluations.forEach(function (evaluation) {
-																donneesEvaluations.push(JSON.parse(evaluation))
-															})
-															donnees.evaluations = donneesEvaluations.length
-															donnees.listeEvaluations = donneesEvaluations
-															resolve(donnees)
+														const donneesEvaluations = []
+														evaluations.forEach(function (evaluation) {
+															donneesEvaluations.push(JSON.parse(evaluation))
 														})
+														donnees.evaluations = donneesEvaluations.length
+														donnees.listeEvaluations = donneesEvaluations
+														resolve(donnees)
 													})
-												} else {
-													resolve({})
-												}
-											})
-										})
-										donneesBlocs.push(donneesBloc)
-									}
-									Promise.all(donneesBlocs).then(function (resultat) {
-										resolveMain(resultat)
-									})
-								})
-							})
-							const activitePad = new Promise(function (resolveMain) {
-								const donneesEntrees = []
-								db.zrange('activite:' + id, 0, -1, function (err, entrees) {
-									if (err) { resolveMain(donneesEntrees) }
-									for (let entree of entrees) {
-										entree = JSON.parse(entree)
-										const donneesEntree = new Promise(function (resolve) {
-											db.exists('utilisateurs:' + entree.identifiant, function (err) {
-												if (err) { resolve({}) }
-												resolve(entree)
-											})
-										})
-										donneesEntrees.push(donneesEntree)
-									}
-									Promise.all(donneesEntrees).then(function (resultat) {
-										resolveMain(resultat)
-									})
-								})
-							})
-							Promise.all([donneesPad, blocsPad, activitePad]).then(function (donnees) {
-								if (donnees.length > 0 && donnees[0].id) {
-									const parametres = {}
-									parametres.pad = donnees[0]
-									parametres.blocs = donnees[1]
-									parametres.activite = donnees[2]
-									fs.writeFile(path.normalize(chemin + '/' + id + '.json'), JSON.stringify(parametres, '', 4), 'utf8', function () {
-										fs.writeFile(path.normalize(chemin + '/pad-' + id + '.json'), JSON.stringify(parametres.pad, '', 4), 'utf8', function () {
-											// Suppression données redis
-											db.zrange('blocs:' + id, 0, -1, function (err, blocs) {
-												const multi = db.multi()
-												for (let i = 0; i < blocs.length; i++) {
-													multi.del('commentaires:' + blocs[i])
-													multi.del('evaluations:' + blocs[i])
-													multi.del('pad-' + id + ':' + blocs[i])
-												}
-												multi.del('blocs:' + id)
-												multi.del('pads:' + id)
-												multi.del('activite:' + id)
-												multi.exec()
-											})
+												})
+											} else {
+												resolve({})
+											}
 										})
 									})
+									donneesBlocs.push(donneesBloc)
 								}
+								Promise.all(donneesBlocs).then(function (resultat) {
+									resolveMain(resultat)
+								})
 							})
-						}
-					})
+						})
+						const activitePad = new Promise(function (resolveMain) {
+							const donneesEntrees = []
+							db.zrange('activite:' + id, 0, -1, function (err, entrees) {
+								if (err) { resolveMain(donneesEntrees) }
+								for (let entree of entrees) {
+									entree = JSON.parse(entree)
+									const donneesEntree = new Promise(function (resolve) {
+										db.exists('utilisateurs:' + entree.identifiant, function (err) {
+											if (err) { resolve({}) }
+											resolve(entree)
+										})
+									})
+									donneesEntrees.push(donneesEntree)
+								}
+								Promise.all(donneesEntrees).then(function (resultat) {
+									resolveMain(resultat)
+								})
+							})
+						})
+						Promise.all([donneesPad, blocsPad, activitePad]).then(function (donnees) {
+							if (donnees.length > 0 && donnees[0].id) {
+								const parametres = {}
+								parametres.pad = donnees[0]
+								parametres.blocs = donnees[1]
+								parametres.activite = donnees[2]
+								fs.writeFile(path.normalize(chemin + '/' + id + '.json'), JSON.stringify(parametres, '', 4), 'utf8', function () {
+									fs.writeFile(path.normalize(chemin + '/pad-' + id + '.json'), JSON.stringify(parametres.pad, '', 4), 'utf8', function () {
+										// Suppression données redis
+										db.zrange('blocs:' + id, 0, -1, function (err, blocs) {
+											const multi = db.multi()
+											for (let i = 0; i < blocs.length; i++) {
+												multi.del('commentaires:' + blocs[i])
+												multi.del('evaluations:' + blocs[i])
+												multi.del('pad-' + id + ':' + blocs[i])
+											}
+											multi.del('blocs:' + id)
+											multi.del('pads:' + id)
+											multi.del('activite:' + id)
+											multi.exec()
+										})
+									})
+								})
+							}
+						})
+					}
 				})
 			})
 		}
