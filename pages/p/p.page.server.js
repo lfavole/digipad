821baa75544bb16c2211ea5b6ddb43c9a34ad0fe
Emmanuel ZIMMERT <ez@ladigitale.dev>
2023-06-20 06:54:27 +0200

    Update p.page.server.js

--- pages/p/p.page.server.js
+++ pages/p/p.page.server.js
@@ -7,8 +7,8 @@ async function onBeforeRender (pageContext) {
 	let redirection = '/'
 	const id = pageContext.routeParams.id
 	const token = pageContext.routeParams.token
-	const identifiant = pageContext.identifiant
-	const statut = pageContext.statut
+	let identifiant = pageContext.identifiant
+	let statut = pageContext.statut
 	const reponse = await axios.post(pageContext.hote + '/api/recuperer-donnees-pad', {
 		id: id,
 		token: token,
@@ -28,6 +28,31 @@ async function onBeforeRender (pageContext) {
 		}
 		pageProps = { redirection }
 	} else {
+		const params = pageContext.params
+		const hote = pageContext.hote
+		const userAgent = pageContext.userAgent
+		const langues = pageContext.langues
+		let nom, langue, digidrive
+		const id = params.id
+		const mdp = params.mdp
+		if (id && id !== '' && mdp && mdp !== '') {
+			const rep = await axios.post(hote + '/api/verifier-acces', {
+				pad: reponse.data.pad.id,
+				identifiant: id,
+				motdepasse: atob(mdp)
+			})
+			if (rep.data.hasOwnProperty('message') && rep.data.message === 'pad_debloque') {
+				identifiant = id
+				nom = rep.data.nom
+				langue = rep.data.langue
+				statut = 'auteur'
+				digidrive = rep.data.digidrive
+			}
+		} else {
+			nom = pageContext.nom
+			langue = pageContext.langue
+			digidrive = pageContext.digidrive
+		}
 		let admin = false
 		if ((reponse.data.pad.hasOwnProperty('identifiant') && reponse.data.pad.identifiant === identifiant) || (reponse.data.pad.hasOwnProperty('admins') && reponse.data.pad.admins.includes(identifiant))) {
 			admin = true
@@ -38,16 +63,8 @@ async function onBeforeRender (pageContext) {
 		} else if (!admin && reponse.data.pad.acces === 'prive' && statut !== 'utilisateur') {
 			pageProps = { redirection }
 		} else {
-			const params = pageContext.params
-			const hote = pageContext.hote
-			const userAgent = pageContext.userAgent
-			const langues = pageContext.langues
-			const nom = pageContext.nom
-			const langue = pageContext.langue
-			const statut = pageContext.statut
 			const acces = pageContext.acces
 			const pads = pageContext.pads
-			const digidrive = pageContext.digidrive
 			const pad = reponse.data.pad
 			const blocs = reponse.data.blocs
 			const activite = reponse.data.activite
