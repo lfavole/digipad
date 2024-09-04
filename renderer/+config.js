94643ad3ee4f4d602fdb235ded96c6ba3ebe973b
Emmanuel ZIMMERT <ez@ladigitale.dev>
2024-09-04 20:59:48 +0200

    Update +config.js

--- renderer/+config.js
+++ renderer/+config.js
@@ -2,8 +2,8 @@ export default {
     passToClient: ['pageProps'],
     hooksTimeout: {
         data: {
-            warning: 5 * 1000,
-            error: 40 * 1000
+            warning: 10 * 1000,
+            error: 60 * 1000
         }
     }
 }
